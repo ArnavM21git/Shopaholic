2671e45e1d77a20e276676b7f470f8c2ee293d43
Arnav Mundada <arnav.mundada@vit.edu.in>
2025-09-02 22:51:50 +0530

    ✨ feat: implement middleware with Supabase client; add cookie handling and session retrieval

---
+++ middleware.js
@@ -0,0 +1,42 @@
+'use server'
+
+import { createServerClient } from '@supabase/ssr'
+import { NextResponse } from 'next/server'
+
+// Force experimental edge runtime
+/** @type {'experimental-edge'} */
+export const runtime = 'experimental-edge'
+
+export async function middleware(request) {
+  const res = NextResponse.next()
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    {
+      cookies: {
+        get: (name) => request.cookies.get(name)?.value,
+        set: (name, value, options) => {
+          res.cookies.set({
+            name,
+            value,
+            ...options,
+          })
+        },
+        remove: (name, options) => {
+          res.cookies.set({
+            name,
+            value: '',
+            ...options,
+          })
+        },
+      },
+    }
+  )
+
+  await supabase.auth.getSession()
+  return res
+}
+
+export const config = {
+  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+}
